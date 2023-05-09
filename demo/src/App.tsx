6153448b3e47a02cd690f7953434f4280c00db8e
Steven DeMartini <sjdemartini@users.noreply.github.com>
2023-05-09 10:33:01 -0700

    Add default example content to demo app, with baseline MUI styles

--- demo/src/App.tsx
+++ demo/src/App.tsx
@@ -1,24 +1,31 @@
-import { Typography } from "@mui/material";
+import { CssBaseline, Typography } from "@mui/material";
 import { useMuiEditor, MuiEditorContent } from "mui-tiptap";
+
+const exampleContent =
+  '<h2>Hey there 👋</h2><p>This is a <em>basic</em> example of using <a target="_blank" rel="noopener noreferrer nofollow" href="https://tiptap.dev/">Tiptap</a> with <a target="_blank" rel="noopener noreferrer nofollow" href="https://mui.com/">MUI (Material-UI)</a>. Sure, there are all kind of <strong>basic text styles</strong> you’d probably expect from a text editor. But wait until you see the lists:</p><ul><li><p>That’s a bullet list with one …</p></li><li><p>… or two list items.</p></li></ul><p>Isn’t that great? And all of that is editable. But wait, there’s more. Let’s try a code block:</p><pre><code class="language-css">body {\n  display: none;\n}</code></pre><p></p><p>It’s only the tip of the iceberg though. Give it a try and click a little bit around. And feel free to add and resize cat photos:</p><img height="auto" src="http://placekitten.com/g/500" alt="wat" width="257" style="aspect-ratio: 1 / 1"><p></p><p>Organize information in tables:</p><table><tbody><tr><th colspan="1" rowspan="1"><p>Name</p></th><th colspan="1" rowspan="1"><p>Role</p></th><th colspan="1" rowspan="1"><p>Team</p></th></tr><tr><td colspan="1" rowspan="1"><p>Alice</p></td><td colspan="1" rowspan="1"><p>PM</p></td><td colspan="1" rowspan="1"><p>Internal tools</p></td></tr><tr><td colspan="1" rowspan="1"><p>Bob</p></td><td colspan="1" rowspan="1"><p>Software</p></td><td colspan="1" rowspan="1"><p>Infrastructure</p></td></tr></tbody></table><p></p><p>Or write down your groceries:</p><ul data-type="taskList"><li data-checked="true" data-type="taskItem"><label><input type="checkbox" checked="checked"><span></span></label><div><p>Milk</p></div></li><li data-checked="false" data-type="taskItem"><label><input type="checkbox"><span></span></label><div><p>Eggs</p></div></li><li data-checked="false" data-type="taskItem"><label><input type="checkbox"><span></span></label><div><p>Sriracha</p></div></li></ul><blockquote><p>Wow, that’s amazing. Good work, boy! 👏 <br>— Mom</p></blockquote><p><strong>Give it a try!</strong></p>';
 
 export default function App() {
   const editor = useMuiEditor({
-    content: "",
+    content: exampleContent,
   });
 
   return (
-    <div>
-      Using the editor!
-      <div style={{ marginTop: 10 }}>
-        <MuiEditorContent editor={editor} showFormattingMenuBar />
+    <>
+      <CssBaseline />
+
+      <div>
+        Using the editor!
+        <div style={{ marginTop: 10 }}>
+          <MuiEditorContent editor={editor} showFormattingMenuBar />
+        </div>
+        <div style={{ marginBottom: 20 }}>
+          <hr />
+        </div>
+        <Typography variant="h5">HTML result:</Typography>
+        <pre style={{ marginTop: 10, overflow: "scroll", maxWidth: "100%" }}>
+          <code>{editor?.getHTML()}</code>
+        </pre>
       </div>
-      <div style={{ marginBottom: 20 }}>
-        <hr />
-      </div>
-      <Typography variant="h5">HTML result:</Typography>
-      <pre style={{ marginTop: 10 }}>
-        <code>{editor?.getHTML()}</code>
-      </pre>
-    </div>
+    </>
   );
 }
--- demo/src/main.tsx
+++ demo/src/main.tsx
@@ -1,11 +1,15 @@
-import React from "react";
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
+import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App";
 
 // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
 const container = document.getElementById("root")!;
 createRoot(container).render(
-  <React.StrictMode>
+  <StrictMode>
     <App />
-  </React.StrictMode>
+  </StrictMode>
 );
