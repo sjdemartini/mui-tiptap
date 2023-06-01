57943f0df34f64066c2517d17627ddc3e9485ee9
Steven DeMartini <sjdemartini@users.noreply.github.com>
2023-06-01 11:06:01 -0700

    Move all link bubble menu state to within extension

--- src/LinkBubbleMenu.tsx
+++ src/LinkBubbleMenu.tsx
@@ -1,102 +1,43 @@
 import { Box } from "@mui/material";
 import type { Editor } from "@tiptap/core";
-import { useEffect, useState } from "react";
 import ControlledBubbleMenu from "./ControlledBubbleMenu";
 import EditLinkMenuContent from "./EditLinkMenuContent";
 import ViewLinkMenuContent from "./ViewLinkMenuContent";
-import type { LinkBubbleMenuHandlerStorage } from "./extensions/LinkBubbleMenuHandler";
+import {
+  LinkMenuState,
+  type LinkBubbleMenuHandlerStorage,
+} from "./extensions/LinkBubbleMenuHandler";
 
 export type LinkBubbleMenuProps = {
   editor: Editor;
 };
-
-enum LinkMenuStep {
-  HIDDEN,
-  VIEW_LINK_DETAILS,
-  EDIT_LINK,
-}
 
 /**
  * A hook for providing a menu for viewing, creating, or editing a link in a
  * Tiptap editor.
  */
 export default function LinkBubbleMenu({ editor }: LinkBubbleMenuProps) {
-  const [menuStep, setMenuStep] = useState(LinkMenuStep.HIDDEN);
-
-  const handlerStorage = editor.storage
-    .linkBubbleMenuHandler as LinkBubbleMenuHandlerStorage;
-
   if (!("linkBubbleMenuHandler" in editor.extensionStorage)) {
     throw new Error(
       "You must add the LinkBubbleMenuHandler extension to the useEditor `extensions` array in order to use this component!"
     );
   }
+  const handlerStorage = editor.storage
+    .linkBubbleMenuHandler as LinkBubbleMenuHandlerStorage;
 
   // Update the menu step if the bubble menu state has changed
-  const isTriggeredToOpenViaHandlerExtension = handlerStorage.open;
-  useEffect(() => {
-    function handleShowLinkMenu() {
-      if (editor.isDestroyed) {
-        return;
-      }
-
-      setMenuStep((currentMenuStep) => {
-        if (editor.isActive("link")) {
-          // If their cursor is currently on a link, we'll open the link menu to
-          // view the details.
-          if (currentMenuStep !== LinkMenuStep.VIEW_LINK_DETAILS) {
-            // If the user isn't already in the "View Link Details" menu, we'll first
-            // change the selection to encompass the entire link to make it obvious which
-            // link is being edited and what text it includes. We also focus in case the
-            // user clicked the Link menu button (so we re-focus on the editor).
-
-            // NOTE: there is a bug in Tiptap where `extendMarkRange` will not
-            // work despite `isActive("link")` having returning true if the
-            // click/cursor is at the end of a link
-            // https://github.com/ueberdosis/tiptap/issues/2535. This leads to
-            // confusing behavior and should probably be handled with a workaround
-            // (like checking whether `extendMarkRange` had any effect) so that we
-            // don't open the link menu unless we know we've selected the entire
-            // link.
-            editor.chain().extendMarkRange("link").focus().run();
-          }
-
-          return LinkMenuStep.VIEW_LINK_DETAILS;
-        }
-
-        // Otherwise open the edit link menu for the user to add a new link
-        return LinkMenuStep.EDIT_LINK;
-      });
-    }
-
-    function handleCancelLinkEdit() {
-      setMenuStep((previousStep) => {
-        const wasEditing = previousStep !== LinkMenuStep.HIDDEN;
-        if (wasEditing) {
-          // Re-focus on the editor (e.g. for re-selection) if the user was
-          // previously editing and has now canceled
-          editor.commands.focus();
-        }
-        return LinkMenuStep.HIDDEN;
-      });
-    }
-
-    if (isTriggeredToOpenViaHandlerExtension) {
-      handleShowLinkMenu();
-    } else {
-      handleCancelLinkEdit();
-    }
-  }, [isTriggeredToOpenViaHandlerExtension, editor]);
+  const menuState = handlerStorage.state;
 
   let linkMenuContent = null;
-  if (editor.isActive("link") && menuStep === LinkMenuStep.VIEW_LINK_DETAILS) {
+  if (
+    editor.isActive("link") &&
+    menuState === LinkMenuState.VIEW_LINK_DETAILS
+  ) {
     linkMenuContent = (
       <ViewLinkMenuContent
         editor={editor}
         onCancel={editor.commands.closeLinkBubbleMenu}
-        onEdit={() => {
-          setMenuStep(LinkMenuStep.EDIT_LINK);
-        }}
+        onEdit={editor.commands.editLinkInBubbleMenu}
         onRemove={() => {
           // Remove the link and place the cursor at the end of the link (which
           // requires "focus" to take effect)
@@ -109,7 +50,7 @@ export default function LinkBubbleMenu({ editor }: LinkBubbleMenuProps) {
         }}
       />
     );
-  } else if (menuStep === LinkMenuStep.EDIT_LINK) {
+  } else if (menuState === LinkMenuState.EDIT_LINK) {
     linkMenuContent = (
       <EditLinkMenuContent
         editor={editor}
@@ -156,14 +97,14 @@ export default function LinkBubbleMenu({ editor }: LinkBubbleMenuProps) {
 
   // We'll show the link bubble menu as open if the user is currently adding/editing a link
   let shouldShow: boolean;
-  if (menuStep === LinkMenuStep.HIDDEN) {
+  if (menuState === LinkMenuState.HIDDEN) {
     shouldShow = false;
-  } else if (menuStep === LinkMenuStep.VIEW_LINK_DETAILS) {
+  } else if (menuState === LinkMenuState.VIEW_LINK_DETAILS) {
     // While we could just set `shouldShow = true;` here and everything would
     // work properly, by checking that a link is currently active under the
     // editor cursor, we can more quickly "close" the bubble menu if someone
     // clicks off (without having to wait for the "cancel" click-handler to set
-    // the menuStep in response to clicking off of a link), to prevent menu
+    // the menuState in response to clicking off of a link), to prevent menu
     // repositioning/flashing before closing. We can only do this for "view"
     // (and not "edit"), since when adding a new link, there is not yet a link
     // at the current position.
@@ -175,7 +116,7 @@ export default function LinkBubbleMenu({ editor }: LinkBubbleMenuProps) {
     // be closed. Otherwise, moving the cursor with the arrow keys away from one
     // link will close the menu, and entering a link again with the keyboard
     // will re-open the menu. (A minor edge case.) Or, this could probably
-    // better be solved by moving the menuStep state from this React component
+    // better be solved by moving the menuState state from this React component
     // instead into the LinkBubbleMenuHandler extension, and having that
     // extension listen to `onUpdate`. Then there aren't two sources of truth
     // for the link menu state.
--- src/extensions/LinkBubbleMenuHandler.ts
+++ src/extensions/LinkBubbleMenuHandler.ts
@@ -10,14 +10,25 @@ declare module "@tiptap/core" {
        * already a link at the current selection.
        */
       openLinkBubbleMenu: () => ReturnType;
+      /**
+       * Edit an existing link in the bubble menu, to be used when currently
+       * viewing a link in the already-opened bubble menu.
+       */
+      editLinkInBubbleMenu: () => ReturnType;
       /** Close/hide the link bubble menu, canceling any ongoing edits. */
       closeLinkBubbleMenu: () => ReturnType;
     };
   }
 }
 
+export enum LinkMenuState {
+  HIDDEN,
+  VIEW_LINK_DETAILS,
+  EDIT_LINK,
+}
+
 export type LinkBubbleMenuHandlerStorage = {
-  open: boolean;
+  state: LinkMenuState;
 };
 
 const LinkBubbleMenuHandler = Extension.create<
@@ -28,27 +39,74 @@ const LinkBubbleMenuHandler = Extension.create<
 
   addStorage() {
     return {
-      open: false,
+      state: LinkMenuState.HIDDEN,
     };
   },
 
   addCommands() {
     return {
-      openLinkBubbleMenu: () => () => {
-        if (this.storage.open) {
-          return false;
-        }
-        this.storage.open = true;
-        return true;
-      },
-      closeLinkBubbleMenu: () => () => {
-        if (!this.storage.open) {
+      openLinkBubbleMenu:
+        () =>
+        ({ editor, chain }) => {
+          const currentMenuState = this.storage.state;
+
+          let newMenuState: LinkMenuState;
+          if (editor.isActive("link")) {
+            // If their cursor is currently on a link, we'll open the link menu to
+            // view the details.
+            if (currentMenuState !== LinkMenuState.VIEW_LINK_DETAILS) {
+              // If the user isn't already in the "View Link Details" menu, we'll first
+              // change the selection to encompass the entire link to make it obvious which
+              // link is being edited and what text it includes. We also focus in case the
+              // user clicked the Link menu button (so we re-focus on the editor).
+
+              // NOTE: there is a bug in Tiptap where `extendMarkRange` will not
+              // work despite `isActive("link")` having returning true if the
+              // click/cursor is at the end of a link
+              // https://github.com/ueberdosis/tiptap/issues/2535. This leads to
+              // confusing behavior and should probably be handled with a workaround
+              // (like checking whether `extendMarkRange` had any effect) so that we
+              // don't open the link menu unless we know we've selected the entire
+              // link.
+              chain().extendMarkRange("link").focus().run();
+            }
+
+            newMenuState = LinkMenuState.VIEW_LINK_DETAILS;
+          } else {
+            // Otherwise open the edit link menu for the user to add a new link
+            newMenuState = LinkMenuState.EDIT_LINK;
+          }
+
+          this.storage.state = newMenuState;
+          return true;
+        },
+
+      editLinkInBubbleMenu: () => () => {
+        const currentMenuState = this.storage.state;
+        const newMenuState = LinkMenuState.EDIT_LINK;
+        if (currentMenuState === newMenuState) {
           return false;
         }
 
-        this.storage.open = false;
+        this.storage.state = newMenuState;
         return true;
       },
+
+      closeLinkBubbleMenu:
+        () =>
+        ({ commands }) => {
+          const currentMenuState = this.storage.state;
+          if (currentMenuState === LinkMenuState.HIDDEN) {
+            return false;
+          }
+
+          // Re-focus on the editor (e.g. for re-selection) since the user was
+          // previously editing and has now canceled
+          commands.focus();
+
+          this.storage.state = LinkMenuState.HIDDEN;
+          return true;
+        },
     };
   },
 
@@ -71,11 +129,16 @@ const LinkBubbleMenuHandler = Extension.create<
             const link = (event.target as HTMLElement).closest("a");
             // If the user has clicked on a link and the menu isn't already
             // open, we'll open it. Otherwise we close it. (Closing the menu if
-            // it's already open allows a user to put their cursor within the
-            // link text and implicitly close the bubble menu, like the Slack UI
-            // does, if they don't want to use the bubble menu but instead want
-            // to use regular cursor/keyboard interaction with the text.)
-            if (link && attrs.href && !this.storage.open) {
+            // it's already open allows a user to put their cursor at a specific
+            // point within the link text and implicitly close the bubble menu,
+            // like the Slack UI does, if they don't want to use the bubble menu
+            // but instead want to use regular cursor/keyboard interaction with
+            // the link text.)
+            if (
+              link &&
+              attrs.href &&
+              this.storage.state === LinkMenuState.HIDDEN
+            ) {
               this.editor.commands.openLinkBubbleMenu();
             } else {
               this.editor.commands.closeLinkBubbleMenu();
