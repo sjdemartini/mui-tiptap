62d9e32e4383b77bad17aaaeac5615764c233a08
Steven DeMartini <sjdemartini@users.noreply.github.com>
2023-06-11 20:52:14 -0700

    Add better documentation for menu button tooltip props

--- src/controls/MenuButton.tsx
+++ src/controls/MenuButton.tsx
@@ -6,8 +6,28 @@ import MenuButtonTooltip, {
 } from "./MenuButtonTooltip";
 
 export type MenuButtonProps = {
+  /**
+   * The label that will be displayed in a tooltip when hovering. Also used as
+   * the underlying ToggleButton `value` if a separate `value` prop is not
+   * included.
+   */
   tooltipLabel: MenuButtonTooltipProps["label"];
+  /**
+   * (Optional) An array of the keyboard shortcut keys that trigger this action
+   * will be displayed in a tooltip when hovering. If empty, no keyboard
+   * shortcut is displayed.
+   *
+   * Use the literal string "mod" to represent Cmd on Mac and Ctrl on Windows
+   * and Linux.
+   *
+   * Example: ["mod", "Shift", "7"] is the array that should be provided as the
+   * combination for toggling an ordered list.
+   *
+   * For the list of pre-configured Tiptap shortcuts, see
+   * https://tiptap.dev/api/keyboard-shortcuts.
+   */
   tooltipShortcutKeys?: MenuButtonTooltipProps["shortcutKeys"];
+  /** The icon component to use for the button. Must accept a className. */
   IconComponent: React.ElementType<{ className: string }>;
 } & SetOptional<ToggleButtonProps, "value">;
 
