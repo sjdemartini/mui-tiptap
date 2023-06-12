c0abf889d2a1516b74d3df3ea1687a2168f68e6d
Steven DeMartini <sjdemartini@users.noreply.github.com>
2023-06-12 13:20:47 -0700

    Add error messaging if editor not provided

--- src/context.ts
+++ src/context.ts
@@ -1,8 +1,17 @@
 import type { Editor } from "@tiptap/react";
 import { createContext, useContext } from "react";
 
-export const RichTextEditorContext = createContext<Editor | null>(null);
+export const RichTextEditorContext = createContext<Editor | null | undefined>(
+  undefined
+);
 
 export function useRichTextEditorContext(): Editor | null {
-  return useContext(RichTextEditorContext);
+  const editor = useContext(RichTextEditorContext);
+  if (editor === undefined) {
+    throw new Error(
+      "Tiptap editor not found in component context. Be sure to use <RichTextEditorProvider editor={editor} />!"
+    );
+  }
+
+  return editor;
 }
