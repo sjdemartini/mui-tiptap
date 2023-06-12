418a03a6f2f3a9b87a6b139020c5744eac4b976a
Steven DeMartini <sjdemartini@users.noreply.github.com>
2023-06-11 20:52:14 -0700

    Add better documentation for menu button tooltip props

--- src/controls/MenuButtonTooltip.tsx
+++ src/controls/MenuButtonTooltip.tsx
@@ -2,13 +2,38 @@ import { Tooltip, Typography, alpha, type TooltipProps } from "@mui/material";
 import { makeStyles } from "tss-react/mui";
 import { getModShortcutKey } from "../utils/platform";
 
-export interface MenuButtonTooltipProps {
+export type MenuButtonTooltipProps = {
+  /**
+   * Used to display what this button is responsible for. Ex: "Ordered list".
+   */
   label: string;
+  /**
+   * An array representing the set of keys that should be pressed to trigger
+   * this action (for its keyboard shortcut), so that this can be displayed to
+   * the user. If empty, no keyboard shortcut is displayed.
+   *
+   * Use the literal string "mod" to represent Cmd on Mac and Ctrl on Windows
+   * and Linux.
+   *
+   * Example: ["mod", "Shift", "7"] is the array that should be provided as the
+   * combination for toggling an ordered list.
+   *
+   * For the list of pre-configured Tiptap shortcuts, see
+   * https://tiptap.dev/api/keyboard-shortcuts.
+   */
   shortcutKeys?: string[];
+  /** Where the tooltip should be placed. By default "top" (above). */
   placement?: TooltipProps["placement"];
+  /**
+   * Class applied to the element that contains the children content. We add an
+   * intermediary element since Tooltip requires a non-disabled child element in
+   * order to render, and we want to allow tooltips to show up even when buttons
+   * are disabled.
+   */
   contentWrapperClassName?: string;
+  /** The menu element for which we're showing a tooltip when hovering. */
   children: React.ReactNode;
-}
+};
 
 const useStyles = makeStyles({ name: { MenuButtonTooltip } })((theme) => ({
   titleContainer: {
