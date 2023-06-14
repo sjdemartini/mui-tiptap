2fee4ec91f8a8ce489b12d457c410b21ffae7cfc
Steven DeMartini <sjdemartini@users.noreply.github.com>
2023-06-13 17:52:49 -0700

    Update vitest configuration to report useful coverage info

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,16 @@
 /// <reference types="vitest" />
 import react from "@vitejs/plugin-react-swc";
 import { defineConfig } from "vite";
+import { configDefaults } from "vitest/config";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  test: {
+    exclude: [...configDefaults.exclude, "example/**"],
+    coverage: {
+      all: true,
+      exclude: [...(configDefaults.coverage.exclude ?? []), "example/**"],
+    },
+  },
 });
