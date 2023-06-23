19e4b4a488538f2f3dba677f178bb2337fac886a
Steven DeMartini <sjdemartini@users.noreply.github.com>
2023-06-23 09:42:49 -0700

    Anchor link bubble menu to link button when opening via button
    Resolves https://github.com/sjdemartini/mui-tiptap/issues/53

--- src/controls/MenuButtonEditLink.tsx
+++ src/controls/MenuButtonEditLink.tsx
@@ -1,4 +1,5 @@
 import { Link } from "@mui/icons-material";
+import { useRef } from "react";
 import { useRichTextEditorContext } from "../context";
 import MenuButton, { type MenuButtonProps } from "./MenuButton";
 
@@ -6,14 +7,20 @@ export type MenuButtonEditLinkProps = Partial<MenuButtonProps>;
 
 export default function MenuButtonEditLink(props: MenuButtonEditLinkProps) {
   const editor = useRichTextEditorContext();
+  const buttonRef = useRef<HTMLButtonElement | null>(null);
   return (
     <MenuButton
+      buttonRef={buttonRef}
       tooltipLabel="Link"
       tooltipShortcutKeys={["mod", "Shift", "U"]}
       IconComponent={Link}
       selected={editor?.isActive("link")}
       disabled={!editor?.isEditable}
-      onClick={editor?.commands.openLinkBubbleMenu}
+      onClick={() =>
+        // Anchor the link bubble menu to the button, when clicking the button
+        // to open it
+        editor?.commands.openLinkBubbleMenu({ anchorEl: buttonRef.current })
+      }
       {...props}
     />
   );
