7869bfa309dc6e6b98c1bc5d77b30750a194ea8d
Steven DeMartini <sjdemartini@users.noreply.github.com>
2023-06-28 12:06:20 -0700

    Add support for text-align with ResizableImage

--- src/extensions/ResizableImageComponent.tsx
+++ src/extensions/ResizableImageComponent.tsx
@@ -4,14 +4,17 @@ import type { Node as ProseMirrorNode } from "@tiptap/pm/model";
 import { NodeViewWrapper } from "@tiptap/react";
 import throttle from "lodash/throttle";
 import { useCallback, useEffect, useMemo, useRef, useState } from "react";
+import { makeStyles } from "tss-react/mui";
 
 // Based on
 // https://github.com/ueberdosis/tiptap/blob/ab4a0e2507b4b92c46d293a0bb06bb00a04af6e0/packages/extension-image/src/image.ts#L47-L59
-type ImageNodeAttributes = {
+// We extend Record<string, unknown>, since we may inherit other global
+// attributes as well, aligned with ProseMirrorNode.attrs typing.
+interface ImageNodeAttributes extends Record<string, unknown> {
   src: string;
   alt?: string | null;
   title?: string | null;
-};
+}
 
 interface ResizableImageNodeAttributes extends ImageNodeAttributes {
   width: string | number | null;
@@ -91,12 +94,24 @@ function Resizer({ onResize }: ResizerProps) {
   );
 }
 
+const useStyles = makeStyles({ name: { ResizableImageComponent } })({
+  imageContainer: {
+    // Use inline-block so that the container is only as big as the inner
+    // img
+    display: "inline-block",
+    // Use relative position so that the resizer is positioned relative to
+    // the img dimensions (via their common container)
+    position: "relative",
+  },
+});
+
 function ResizableImageComponent({
   editor,
   node,
   selected,
   updateAttributes,
 }: Props) {
+  const { classes } = useStyles();
   const { attrs } = node;
 
   const theme = useTheme();
@@ -156,73 +171,82 @@ function ResizableImageComponent({
   return (
     <NodeViewWrapper
       style={{
-        // Use inline-block so that the wrapper component is only as big as the
-        // inner img
-        display: "inline-block",
-        // Use relative position so that the resizer is positioned relative to
-        // the img dimensions (via their common container)
-        position: "relative",
+        // Handle @tiptap/extension-text-align. Ideally we'd be able to inherit
+        // this style from TextAlign's GlobalAttributes directly, but those are
+        // only applied via `renderHTML` and not the `NodeView` renderer
+        // (https://github.com/ueberdosis/tiptap/blob/6c34dec33ac39c9f037a0a72e4525f3fc6d422bf/packages/extension-text-align/src/text-align.ts#L43-L49),
+        // so we have to do this manually/redundantly here.
+        textAlign: attrs.textAlign,
+        width: "100%",
       }}
     >
-      <img
-        ref={imageRef}
-        src={attrs.src}
-        height="auto"
-        width={attrs.width ? attrs.width : undefined}
-        {...{
-          // eslint-disable-next-line @typescript-eslint/prefer-nullish-coalescing
-          alt: attrs.alt || undefined,
-          // eslint-disable-next-line @typescript-eslint/prefer-nullish-coalescing
-          title: attrs.title || undefined,
-        }}
-        style={{
-          // We need display:block in order for the container element to be
-          // sized properly (no extra space below the image)
-          display: "block",
-          // If no width has been specified, we use auto max-width
-          maxWidth: attrs.width ? undefined : "auto",
-          // Always specify the aspect-ratio if it's been defined, to improve
-          // initial render (so auto-height works before the image loads)
-          aspectRatio: attrs.aspectRatio ?? undefined,
-          // This "selected" state outline style is copied from our standard
-          // editor styles (which are kept there as well so they appear even if
-          // not using our custom resizable image). We'll only show this when
-          // the editor content is editable, though.
-          outline: isSelectedAndEditable
-            ? `3px solid ${theme.palette.primary.main}`
-            : undefined,
-        }}
-        // For consistency with the standard Image extension selection class/UI (but
-        // only when editable)
-        className={
-          isSelectedAndEditable ? "ProseMirror-selectednode" : undefined
-        }
-        // To make this image act as the drag handle for moving it within the
-        // document, add the data-drag-handle used by Tiptap
-        // (https://tiptap.dev/guide/node-views/react#dragging)
-        data-drag-handle
-        // When the image loads, we'll update our width and aspect-ratio based
-        // on the image's natural size, if they're not set. That way, all future
-        // renders will know the image width/height prior to load/render,
-        // preventing flashing
-        onLoad={(event) => {
-          const newAttributes: Partial<ResizableImageNodeAttributes> = {};
-          if (!attrs.width) {
-            newAttributes.width = event.currentTarget.naturalWidth;
+      {/* We need a separate inner image container here in order to (1) have the
+      node view wrapper take up the full width of its parent div created by
+      ReactNodeViewRender (so we can utilize text-align for these children
+      elements), and (2) still allow for this image container to take up exactly
+      the size of the `img` being rendered, which allows for positioning the
+      resize handle at the edge of the img. */}
+      <div className={classes.imageContainer}>
+        <img
+          ref={imageRef}
+          src={attrs.src}
+          height="auto"
+          width={attrs.width ? attrs.width : undefined}
+          {...{
+            // eslint-disable-next-line @typescript-eslint/prefer-nullish-coalescing
+            alt: attrs.alt || undefined,
+            // eslint-disable-next-line @typescript-eslint/prefer-nullish-coalescing
+            title: attrs.title || undefined,
+          }}
+          style={{
+            // We need display:block in order for the container element to be
+            // sized properly (no extra space below the image)
+            display: "block",
+            // If no width has been specified, we use auto max-width
+            maxWidth: attrs.width ? undefined : "auto",
+            // Always specify the aspect-ratio if it's been defined, to improve
+            // initial render (so auto-height works before the image loads)
+            aspectRatio: attrs.aspectRatio ?? undefined,
+            // This "selected" state outline style is copied from our standard
+            // editor styles (which are kept there as well so they appear even if
+            // not using our custom resizable image). We'll only show this when
+            // the editor content is editable, though.
+            outline: isSelectedAndEditable
+              ? `3px solid ${theme.palette.primary.main}`
+              : undefined,
+          }}
+          // For consistency with the standard Image extension selection class/UI (but
+          // only when editable)
+          className={
+            isSelectedAndEditable ? "ProseMirror-selectednode" : undefined
           }
-          if (!attrs.aspectRatio) {
-            newAttributes.aspectRatio = String(
-              event.currentTarget.naturalWidth /
-                event.currentTarget.naturalHeight
-            );
-          }
-          if (newAttributes.width || newAttributes.aspectRatio) {
-            updateAttributes(newAttributes);
-          }
-        }}
-      />
+          // To make this image act as the drag handle for moving it within the
+          // document, add the data-drag-handle used by Tiptap
+          // (https://tiptap.dev/guide/node-views/react#dragging)
+          data-drag-handle
+          // When the image loads, we'll update our width and aspect-ratio based
+          // on the image's natural size, if they're not set. That way, all future
+          // renders will know the image width/height prior to load/render,
+          // preventing flashing
+          onLoad={(event) => {
+            const newAttributes: Partial<ResizableImageNodeAttributes> = {};
+            if (!attrs.width) {
+              newAttributes.width = event.currentTarget.naturalWidth;
+            }
+            if (!attrs.aspectRatio) {
+              newAttributes.aspectRatio = String(
+                event.currentTarget.naturalWidth /
+                  event.currentTarget.naturalHeight
+              );
+            }
+            if (newAttributes.width || newAttributes.aspectRatio) {
+              updateAttributes(newAttributes);
+            }
+          }}
+        />
 
-      {isSelectedAndEditable && <Resizer onResize={handleResize} />}
+        {isSelectedAndEditable && <Resizer onResize={handleResize} />}
+      </div>
     </NodeViewWrapper>
   );
 }
