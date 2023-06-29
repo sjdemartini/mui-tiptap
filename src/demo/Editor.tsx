9bcc97e95a679df31645568668069dce3fe65589
Steven DeMartini <sjdemartini@users.noreply.github.com>
2023-06-29 08:12:18 -0700

    Update scroll margin for headings in example

--- src/demo/Editor.tsx
+++ src/demo/Editor.tsx
@@ -1,5 +1,5 @@
 import { Lock, LockOpen, TextFields } from "@mui/icons-material";
-import { Button, Stack, Typography } from "@mui/material";
+import { Box, Button, Stack, Typography } from "@mui/material";
 import { useRef, useState } from "react";
 import LinkBubbleMenu from "../LinkBubbleMenu";
 import RichTextEditor, { type RichTextEditorRef } from "../RichTextEditor";
@@ -23,75 +23,94 @@ export default function Editor() {
 
   return (
     <>
-      <RichTextEditor
-        ref={rteRef}
-        extensions={extensions}
-        content={exampleContent}
-        editable={isEditable}
-        renderControls={() => <EditorMenuControls />}
-        RichTextFieldProps={{
-          MenuBarProps: {
-            hide: !showMenuBar,
+      <Box
+        sx={{
+          // An example of how editor styles can be overridden. In this case,
+          // setting where the scroll anchors to when jumping to headings. The
+          // scroll margin isn't built in since it will likely vary depending on
+          // where the editor itself is rendered (e.g. if there's a sticky nav
+          // bar on your site).
+          "& .ProseMirror": {
+            "& h1, & h2, & h3, & h4, & h5, & h6": {
+              scrollMarginTop: showMenuBar ? 50 : 0,
+            },
           },
-          // Below is an example of adding a toggle within the outlined field
-          // for showing/hiding the editor menu bar, and a "submit" button for
-          // saving/viewing the HTML content
-          footer: (
-            <Stack
-              direction="row"
-              spacing={2}
-              sx={{
-                borderTopStyle: "solid",
-                borderTopWidth: 1,
-                borderTopColor: (theme) => theme.palette.divider,
-                py: 1,
-                px: 1.5,
-              }}
-            >
-              <MenuButton
-                value="formatting"
-                tooltipLabel={
-                  showMenuBar ? "Hide formatting" : "Show formatting"
-                }
-                size="small"
-                onClick={() => setShowMenuBar((currentState) => !currentState)}
-                selected={showMenuBar}
-                IconComponent={TextFields}
-              />
-
-              <MenuButton
-                value="formatting"
-                tooltipLabel={
-                  isEditable
-                    ? "Prevent edits (use read-only mode)"
-                    : "Allow edits"
-                }
-                size="small"
-                onClick={() => setIsEditable((currentState) => !currentState)}
-                selected={!isEditable}
-                IconComponent={isEditable ? Lock : LockOpen}
-              />
-
-              <Button
-                variant="contained"
-                size="small"
-                onClick={() => {
-                  setSubmittedContent(rteRef.current?.editor?.getHTML() ?? "");
-                }}
-              >
-                Save
-              </Button>
-            </Stack>
-          ),
         }}
       >
-        {() => (
-          <>
-            <LinkBubbleMenu />
-            <TableBubbleMenu />
-          </>
-        )}
-      </RichTextEditor>
+        <RichTextEditor
+          ref={rteRef}
+          extensions={extensions}
+          content={exampleContent}
+          editable={isEditable}
+          renderControls={() => <EditorMenuControls />}
+          RichTextFieldProps={{
+            MenuBarProps: {
+              hide: !showMenuBar,
+            },
+            // Below is an example of adding a toggle within the outlined field
+            // for showing/hiding the editor menu bar, and a "submit" button for
+            // saving/viewing the HTML content
+            footer: (
+              <Stack
+                direction="row"
+                spacing={2}
+                sx={{
+                  borderTopStyle: "solid",
+                  borderTopWidth: 1,
+                  borderTopColor: (theme) => theme.palette.divider,
+                  py: 1,
+                  px: 1.5,
+                }}
+              >
+                <MenuButton
+                  value="formatting"
+                  tooltipLabel={
+                    showMenuBar ? "Hide formatting" : "Show formatting"
+                  }
+                  size="small"
+                  onClick={() =>
+                    setShowMenuBar((currentState) => !currentState)
+                  }
+                  selected={showMenuBar}
+                  IconComponent={TextFields}
+                />
+
+                <MenuButton
+                  value="formatting"
+                  tooltipLabel={
+                    isEditable
+                      ? "Prevent edits (use read-only mode)"
+                      : "Allow edits"
+                  }
+                  size="small"
+                  onClick={() => setIsEditable((currentState) => !currentState)}
+                  selected={!isEditable}
+                  IconComponent={isEditable ? Lock : LockOpen}
+                />
+
+                <Button
+                  variant="contained"
+                  size="small"
+                  onClick={() => {
+                    setSubmittedContent(
+                      rteRef.current?.editor?.getHTML() ?? ""
+                    );
+                  }}
+                >
+                  Save
+                </Button>
+              </Stack>
+            ),
+          }}
+        >
+          {() => (
+            <>
+              <LinkBubbleMenu />
+              <TableBubbleMenu />
+            </>
+          )}
+        </RichTextEditor>
+      </Box>
 
       <Typography variant="h5" sx={{ mt: 5 }}>
         Saved result:
