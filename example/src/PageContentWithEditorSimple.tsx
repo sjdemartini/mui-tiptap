7a96bea1dcb0a7c73a866a08e6f25f8112f7897a
Steven DeMartini <sjdemartini@users.noreply.github.com>
2023-07-04 11:47:29 -0700

    Add the README RichTextEditor simple example to example dir

---
+++ example/src/PageContentWithEditorSimple.tsx
@@ -0,0 +1,40 @@
+import { Button } from "@mui/material";
+import { StarterKit } from "@tiptap/starter-kit";
+import {
+  MenuButtonBold,
+  MenuButtonItalic,
+  MenuControlsContainer,
+  MenuDivider,
+  MenuSelectHeading,
+  RichTextEditor,
+  type RichTextEditorRef,
+} from "mui-tiptap";
+import { useRef } from "react";
+
+export default function PageContentWithEditorSimple() {
+  const rteRef = useRef<RichTextEditorRef>(null);
+
+  return (
+    <div>
+      <RichTextEditor
+        ref={rteRef}
+        extensions={[StarterKit]} // Or any Tiptap extensions you wish!
+        content="<p>Hello world</p>"
+        // Optionally include `renderControls` for a menu-bar atop the editor
+        renderControls={() => (
+          <MenuControlsContainer>
+            <MenuSelectHeading />
+            <MenuDivider />
+            <MenuButtonBold />
+            <MenuButtonItalic />
+            {/* Add more controls of your choosing here */}
+          </MenuControlsContainer>
+        )}
+      />
+
+      <Button onClick={() => console.log(rteRef.current?.editor?.getHTML())}>
+        Log HTML
+      </Button>
+    </div>
+  );
+}
