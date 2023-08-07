eec057b222240939441bc432d48ddd84fa9c0797
Steven DeMartini <sjdemartini@users.noreply.github.com>
2023-08-07 12:54:59 -0700

    Use local versions of Remix icons for TableMenuControls

---
+++ src/icons/LayoutColumnFill.tsx
@@ -0,0 +1,9 @@
+import { createSvgIcon } from "@mui/material";
+
+const LayoutColumnFill = createSvgIcon(
+  // From https://remixicon.com/ (https://github.com/Remix-Design/RemixIcon)
+  <path d="M12 5V19H19V5H12ZM4 3H20C20.5523 3 21 3.44772 21 4V20C21 20.5523 20.5523 21 20 21H4C3.44772 21 3 20.5523 3 20V4C3 3.44772 3.44772 3 4 3Z"></path>,
+  "LayoutColumnFill"
+);
+
+export default LayoutColumnFill;
