4378d60ffc3765a2c519c75b5697a2e1062b350c
Steven DeMartini <sjdemartini@users.noreply.github.com>
2023-08-10 16:14:52 -0700

    Allow "reset to default" if multiple font families are selected

--- src/controls/MenuSelectFontFamily.tsx
+++ src/controls/MenuSelectFontFamily.tsx
@@ -5,6 +5,7 @@ import type { ReactNode } from "react";
 import { makeStyles } from "tss-react/mui";
 import type { Except } from "type-fest";
 import { useRichTextEditorContext } from "../context";
+import { getAttributesForEachSelected } from "../utils/getAttributesForEachSelected";
 import MenuSelect, { type MenuSelectProps } from "./MenuSelect";
 
 export type FontFamilySelectOption = {
@@ -50,8 +51,9 @@ export interface MenuSelectFontFamilyProps
    */
   hideUnsetOption?: boolean;
   /**
-   * What to render in the Select when no font-family is currently set for the
-   * selected text. By default shows "Font".
+   * What to render in the Select when either no font-family is currently set
+   * for the selected text, or when multiple different values are set. By
+   * default shows "Font".
    */
   emptyLabel?: React.ReactNode;
 }
@@ -72,6 +74,13 @@ interface TextStyleAttrs extends ReturnType<Editor["getAttributes"]> {
   fontFamily?: string | null;
 }
 
+// Use this as a sentinel value so we can handle the case that the user's
+// selection includes multiple different font families. There won't be a visible
+// "option" in the Select for this value, and this will allow the user to set
+// the current font family to "Default" or to any of the multiple values, and
+// have it take effect. See more comments around `currentFontFamily` below.
+const MULTIPLE_FAMILIES_SELECTED_VALUE = "MULTIPLE";
+
 /** A font-family selector for use with the Tiptap FontFamily extension.  */
 export default function MenuSelectFontFamily({
   options,
@@ -83,9 +92,52 @@ export default function MenuSelectFontFamily({
   const { classes, cx } = useStyles();
   const editor = useRichTextEditorContext();
 
-  const currentAttrs: TextStyleAttrs | undefined =
-    editor?.getAttributes("textStyle");
-  const currentFontFamily = currentAttrs?.fontFamily;
+  // Determine if all of the selected content shares the same set font family.
+  // Scenarios:
+  // 1) If there is exactly one font family amongst the selected content and all
+  //    of the selected content has the font family set, we'll show that as the
+  //    current Selected value (as a user would expect).
+  // 2) If there are multiple families used in the selected content or some
+  //    selected content has font family set and other content does not, we'll
+  //    assign the Select's `value` to a sentinel variable so that users can
+  //    unset the families or can change to any given family.
+  // 3) Otherwise (no font family is set in any selected content), we'll show the
+  //    unsetOption as selected.
+  const allCurrentTextStyleAttrs: TextStyleAttrs[] = editor
+    ? getAttributesForEachSelected(editor.state, "textStyle")
+    : [];
+  const isTextStyleAppliedToEntireSelection = !!editor?.isActive("textStyle");
+  const currentFontFamilies: string[] = allCurrentTextStyleAttrs.map(
+    (attrs) => attrs.fontFamily ?? "" // Treat any null/missing font-family as ""
+  );
+  if (!isTextStyleAppliedToEntireSelection) {
+    // If there is some selected content that does not have textStyle, we can
+    // treat it the same as a selected textStyle mark with fontFamily set to
+    // null or ""
+    currentFontFamilies.push("");
+  }
+  const numUniqueCurrentFontFamilies = new Set(currentFontFamilies).size;
+
+  let currentFontFamily;
+  if (numUniqueCurrentFontFamilies === 1) {
+    // There's exactly one font family selected, so show that
+    currentFontFamily = currentFontFamilies[0];
+  } else if (numUniqueCurrentFontFamilies > 1) {
+    // There are multiple font families (either explicitly, or because some of the
+    // selection has a font family set and some does not). This is similar to what
+    // Microsoft Word and Google Docs do, for instance, showing the font family
+    // input as blank when there are multiple values. If we simply set
+    // currentFontFamily as "" here, then the "unset option" would show as
+    // selected, which would prevent the user from unsetting the font families
+    // for the selected content (since Select onChange does not fire when the
+    // currently selected option is chosen again).
+    currentFontFamily = MULTIPLE_FAMILIES_SELECTED_VALUE;
+  } else {
+    // Show as unset (empty), since there are no font families in any of the
+    // selected content. This will show the "unset option" with the
+    // unsetOptionLabel as selected, if `hideUnsetOption` is false.
+    currentFontFamily = "";
+  }
 
   return (
     <MenuSelect<string>
@@ -102,7 +154,7 @@ export default function MenuSelectFontFamily({
         !editor?.isEditable || !editor.can().setFontFamily("serif")
       }
       renderValue={(value) => {
-        if (!value) {
+        if (!value || value === MULTIPLE_FAMILIES_SELECTED_VALUE) {
           return emptyLabel;
         }
         return options.find((option) => option.value === value)?.label ?? value;
@@ -113,7 +165,6 @@ export default function MenuSelectFontFamily({
       {...menuSelectProps}
       // We don't want to pass any non-string falsy values here, always falling
       // back to ""
-      // eslint-disable-next-line @typescript-eslint/prefer-nullish-coalescing
       value={currentFontFamily || ""}
       inputProps={{
         ...menuSelectProps.inputProps,
@@ -127,6 +178,14 @@ export default function MenuSelectFontFamily({
         // Allow users to unset the font-family
         <MenuItem value="">{unsetOptionLabel}</MenuItem>
       )}
+
+      {/* Including a "hidden" option for "multiple selected" (we don't want a
+      user to be able to select this) allows us to avoid "you have provided an
+      out-of-range value" errors */}
+      <MenuItem
+        style={{ display: "none" }}
+        value={MULTIPLE_FAMILIES_SELECTED_VALUE}
+      />
 
       {options.map((fontFamilyOption) => (
         <MenuItem key={fontFamilyOption.value} value={fontFamilyOption.value}>
