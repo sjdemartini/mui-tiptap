a84e2335169df7b5b09c67a3d6e80049a7f46984
Steven DeMartini <sjdemartini@users.noreply.github.com>
2023-08-10 19:29:27 -0700

    Export/expose new "get attributes" utils

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,4 +1,7 @@
 export { default as DebounceRender } from "./DebounceRender";
+export { getAttributesForEachSelected } from "./getAttributesForEachSelected";
+export { getAttributesForMarks } from "./getAttributesForMarks";
+export { getAttributesForNodes } from "./getAttributesForNodes";
 export { default as keymapPluginFactory } from "./keymapPluginFactory";
 export { getModShortcutKey, isMac, isTouchDevice } from "./platform";
 export { default as slugify } from "./slugify";
