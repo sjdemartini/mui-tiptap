994b1c9d598ca6abe61c327539d2535d6bb5d6b9
Steven DeMartini <sjdemartini@users.noreply.github.com>
2023-08-16 12:43:55 -0700

    Add @mention example to demo
    This should help as example to folks who want to implement their own
    @mention functionality, since the typing is difficult (building upon
    https://github.com/ueberdosis/tiptap/discussions/2274#discussioncomment-5984536).
    
    This also makes it easier to see the built-in styles for @mentions.

---
+++ src/demo/mentionSuggestionOptions.ts
@@ -0,0 +1,141 @@
+import type { MentionOptions } from "@tiptap/extension-mention";
+import { ReactRenderer } from "@tiptap/react";
+import tippy, { type Instance as TippyInstance } from "tippy.js";
+import MentionList, { type MentionListRef } from "./MentionList";
+
+export type MentionSuggestion = {
+  id: string;
+  mentionLabel: string;
+};
+
+/**
+ * Workaround for the current typing incompatibility between Tippy.js and Tiptap
+ * Suggestion utility.
+ *
+ * @see https://github.com/ueberdosis/tiptap/issues/2795#issuecomment-1160623792
+ *
+ * Adopted from
+ * https://github.com/Doist/typist/blob/a1726a6be089e3e1452def641dfcfc622ac3e942/stories/typist-editor/constants/suggestions.ts#L169-L186
+ */
+const DOM_RECT_FALLBACK: DOMRect = {
+  bottom: 0,
+  height: 0,
+  left: 0,
+  right: 0,
+  top: 0,
+  width: 0,
+  x: 0,
+  y: 0,
+  toJSON() {
+    return {};
+  },
+};
+
+export const mentionSuggestionOptions: MentionOptions["suggestion"] = {
+  // Replace this `items` code with a call to your API that returns suggestions
+  // of whatever sort you like (including potentially additional data beyond
+  // just an ID and a label). It need not be async but is written that way for
+  // the sake of example.
+  items: async ({ query }): Promise<MentionSuggestion[]> =>
+    Promise.resolve(
+      [
+        "Lea Thompson",
+        "Cyndi Lauper",
+        "Tom Cruise",
+        "Madonna",
+        "Jerry Hall",
+        "Joan Collins",
+        "Winona Ryder",
+        "Christina Applegate",
+        "Alyssa Milano",
+        "Molly Ringwald",
+        "Ally Sheedy",
+        "Debbie Harry",
+        "Olivia Newton-John",
+        "Elton John",
+        "Michael J. Fox",
+        "Axl Rose",
+        "Emilio Estevez",
+        "Ralph Macchio",
+        "Rob Lowe",
+        "Jennifer Grey",
+        "Mickey Rourke",
+        "John Cusack",
+        "Matthew Broderick",
+        "Justine Bateman",
+        "Lisa Bonet",
+        "Benicio Monserrate Rafael del Toro Sánchez",
+      ]
+        // Typically we'd be getting this data from an API where we'd have a
+        // definitive "id" to use for each suggestion item, but for the sake of
+        // example, we'll just set the index within this hardcoded list as the
+        // ID of each item.
+        .map((name, index) => ({ mentionLabel: name, id: index.toString() }))
+        // Find matching entries based on what the user has typed so far (after
+        // the @ symbol)
+        .filter((item) =>
+          item.mentionLabel.toLowerCase().startsWith(query.toLowerCase())
+        )
+        .slice(0, 5)
+    ),
+
+  render: () => {
+    let component: ReactRenderer<MentionListRef> | undefined;
+    let popup: TippyInstance | undefined;
+
+    return {
+      onStart: (props) => {
+        component = new ReactRenderer(MentionList, {
+          props,
+          editor: props.editor,
+        });
+
+        popup = tippy("body", {
+          getReferenceClientRect: () =>
+            props.clientRect?.() ?? DOM_RECT_FALLBACK,
+          appendTo: () => document.body,
+          content: component.element,
+          showOnCreate: true,
+          interactive: true,
+          trigger: "manual",
+          placement: "bottom-start",
+        })[0];
+      },
+
+      onUpdate(props) {
+        component?.updateProps(props);
+
+        popup?.setProps({
+          getReferenceClientRect: () =>
+            props.clientRect?.() ?? DOM_RECT_FALLBACK,
+        });
+      },
+
+      onKeyDown(props) {
+        if (props.event.key === "Escape") {
+          popup?.hide();
+          return true;
+        }
+
+        if (!component?.ref) {
+          return false;
+        }
+
+        return component.ref.onKeyDown(props);
+      },
+
+      onExit() {
+        popup?.destroy();
+        component?.destroy();
+
+        // Remove references to the old popup and component upon destruction/exit.
+        // (This should prevent redundant calls to `popup.destroy()`, which Tippy
+        // warns in the console is a sign of a memory leak, as the `suggestion`
+        // plugin seems to call `onExit` both when a suggestion menu is closed after
+        // a user chooses an option, *and* when the editor itself is destroyed.)
+        popup = undefined;
+        component = undefined;
+      },
+    };
+  },
+};
