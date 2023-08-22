f14fd331dbb7c8ba3c2e94f5d3df022ecb74bd73
Steven DeMartini <sjdemartini@users.noreply.github.com>
2023-08-22 12:54:22 -0700

    Add note to colorToHex function about third party color-parsing

--- src/utils/color.ts
+++ src/utils/color.ts
@@ -3,6 +3,13 @@ import { rgbToHex } from "@mui/material";
 /**
  * Convert a color string to a color in hex string format (e.g. "#ff0000"), or
  * return null if the given string cannot be parsed as a valid color.
+ *
+ * Separate third party libraries could be used instead of this function to
+ * offer more full-featured parsing (e.g. handling CSS color name keywords,
+ * cmyk, etc.), such as colord (https://www.npmjs.com/package/colord) and
+ * tinycolor2 (https://www.npmjs.com/package/@ctrl/tinycolor), which have a
+ * relatively small footprint. They are not used directly by mui-tiptap to keep
+ * dependencies as lean as possible.
  */
 export function colorToHex(color: string): string | null {
   try {
