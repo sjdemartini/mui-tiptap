781a6324445c5fc9dff4c53928b74bbc4e7d88a6
Steven DeMartini <sjdemartini@users.noreply.github.com>
2023-08-24 21:43:55 -0700

    Add icons that will allow for showing color separately

---
+++ src/icons/FormatColorTextNoBar.tsx
@@ -0,0 +1,15 @@
+import { createSvgIcon } from "@mui/material";
+
+/**
+ * A modified version of the FormatColorText icon from @mui/icons-material, with
+ * the horizontal bar below the "A" letter removed.
+ *
+ * This allows us to control/render the color of the bar independently, via a
+ * separate icon (mui-tipap's FormatColorBar).
+ */
+const FormatColorTextNoBar = createSvgIcon(
+  <path d="M 5.49 17 h 2.42 l 1.27 -3.58 h 5.65 L 16.09 17 h 2.42 L 13.25 3 h -2.5 L 5.49 17 z m 4.42 -5.61 l 2.03 -5.79 h 0.12 l 2.03 5.79 H 9.91 z" />,
+  "FormatColorTextNoBar"
+);
+
+export default FormatColorTextNoBar;
---
+++ src/icons/FormatInkHighlighterNoBar.tsx
@@ -0,0 +1,15 @@
+import { createSvgIcon } from "@mui/material";
+
+/**
+ * A modified version of the FormatInkHighlighter icon, with the horizontal bar
+ * below the highlighter removed.
+ *
+ * This allows us to control/render the color of the bar independently, via a
+ * separate icon (mui-tipap's FormatColorBar).
+ */
+const FormatInkHighlighterNoBar = createSvgIcon(
+  <path d="M 10.6 8 l 5.4 5.425 l -4 4 q -0.6 0.6 -1.413 0.6 t -1.412 -0.6 L 8.5 18 h -5 l 3.15 -3.125 q -0.6 -0.6 -0.625 -1.438 T 6.6 12 l 4 -4 Z M 12 6.575 L 16 2.6 q 0.6 -0.6 1.413 -0.6 t 1.412 0.6 l 2.6 2.575 q 0.6 0.6 0.6 1.413 T 21.425 8 l -4 4 L 12 6.575 Z" />,
+  "FormatInkHighlighterNoBar"
+);
+
+export default FormatInkHighlighterNoBar;
