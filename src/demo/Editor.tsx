fb6ac80f5020c5ae9141f95b5fa3501f31e6d60e
Steven DeMartini <sjdemartini@users.noreply.github.com>
2023-09-10 13:27:30 -0700

    Include image paste and drop examples in demo

--- src/demo/Editor.tsx
+++ src/demo/Editor.tsx
@@ -1,12 +1,14 @@
 import { Lock, LockOpen, TextFields } from "@mui/icons-material";
 import { Box, Button, Stack, Typography } from "@mui/material";
-import { useRef, useState } from "react";
+import type { EditorOptions } from "@tiptap/core";
+import { useCallback, useRef, useState } from "react";
 import {
   LinkBubbleMenu,
   MenuButton,
   RichTextEditor,
   RichTextReadOnly,
   TableBubbleMenu,
+  insertImages,
   type RichTextEditorRef,
 } from "../";
 import EditorMenuControls from "./EditorMenuControls";
@@ -15,6 +17,16 @@ import useExtensions from "./useExtensions";
 const exampleContent =
   '<h2 style="text-align: center">Hey there 👋</h2><p>This is a <em>basic</em> example of <code>mui-tiptap</code>, which combines <a target="_blank" rel="noopener noreferrer nofollow" href="https://tiptap.dev/">Tiptap</a> with customizable <a target="_blank" rel="noopener noreferrer nofollow" href="https://mui.com/">MUI (Material-UI)</a> styles, plus a suite of additional components and extensions! Sure, there are <strong>all <em>kinds</em> of <s>text</s> <u>formatting</u> options</strong> you’d probably expect from a rich text editor. But wait until you see the <span data-type="mention" data-id="15" data-label="Axl Rose">@Axl Rose</span> mentions and lists:</p><ul><li><p>That’s a bullet list with one …</p></li><li><p>… or two list items.</p></li></ul><p>Isn’t that great? And all of that is editable. <strong><span style="color: #ff9900">But wait, </span><span style="color: #403101"><mark data-color="#ffd699" style="background-color: #ffd699; color: inherit">there’s more!</mark></span></strong> Let’s try a code block:</p><pre><code class="language-css">body {\n  display: none;\n}</code></pre><p></p><p>That’s only the tip of the iceberg. Feel free to add and resize images:</p><img height="auto" src="http://placekitten.com/g/500" alt="wat" width="257" style="aspect-ratio: 1 / 1"><p></p><p>Organize information in tables:</p><table><tbody><tr><th colspan="1" rowspan="1"><p>Name</p></th><th colspan="1" rowspan="1"><p>Role</p></th><th colspan="1" rowspan="1"><p>Team</p></th></tr><tr><td colspan="1" rowspan="1"><p>Alice</p></td><td colspan="1" rowspan="1"><p>PM</p></td><td colspan="1" rowspan="1"><p>Internal tools</p></td></tr><tr><td colspan="1" rowspan="1"><p>Bob</p></td><td colspan="1" rowspan="1"><p>Software</p></td><td colspan="1" rowspan="1"><p>Infrastructure</p></td></tr></tbody></table><p></p><p>Or write down your groceries:</p><ul data-type="taskList"><li data-checked="true" data-type="taskItem"><label><input type="checkbox" checked="checked"><span></span></label><div><p>Milk</p></div></li><li data-checked="false" data-type="taskItem"><label><input type="checkbox"><span></span></label><div><p>Eggs</p></div></li><li data-checked="false" data-type="taskItem"><label><input type="checkbox"><span></span></label><div><p>Sriracha</p></div></li></ul><blockquote><p>Wow, that’s amazing. Good work! 👏 <br>— Mom</p></blockquote><p>Give it a try and click around!</p>';
 
+function fileListToImageFiles(fileList: FileList): File[] {
+  // You may want to use a package like attr-accept
+  // (https://www.npmjs.com/package/attr-accept) to restrict to certain file
+  // types.
+  return Array.from(fileList).filter((file) => {
+    const mimeType = (file.type || "").toLowerCase();
+    return mimeType.startsWith("image/");
+  });
+}
+
 export default function Editor() {
   const extensions = useExtensions({
     placeholder: "Add your own content here...",
@@ -22,6 +34,85 @@ export default function Editor() {
   const rteRef = useRef<RichTextEditorRef>(null);
   const [isEditable, setIsEditable] = useState(true);
   const [showMenuBar, setShowMenuBar] = useState(true);
+
+  const handleNewImageFiles = useCallback(
+    (files: File[], insertPosition?: number): void => {
+      if (!rteRef.current?.editor) {
+        return;
+      }
+
+      // For the sake of a demo, we don't have a server to upload the files to,
+      // so we'll instead convert each one to a local "temporary" object URL.
+      // This will not persist properly in a production setting. You should
+      // instead upload the image files to your server, or perhaps convert the
+      // images to bas64 if you would like to encode the image data directly
+      // into the editor content, though that can make the editor content very
+      // large. You will probably want to use the same upload function here as
+      // for the MenuButtonImageUpload `onUploadFiles` prop.
+      const attributesForImageFiles = files.map((file) => ({
+        url: URL.createObjectURL(file),
+        alt: file.name,
+      }));
+
+      insertImages({
+        images: attributesForImageFiles,
+        editor: rteRef.current.editor,
+        insertPosition,
+      });
+    },
+    []
+  );
+
+  // Allow for dropping images into the editor
+  const handleDrop: NonNullable<EditorOptions["editorProps"]["handleDrop"]> =
+    useCallback(
+      (view, event, _slice, _moved) => {
+        if (!(event instanceof DragEvent) || !event.dataTransfer) {
+          return false;
+        }
+
+        const imageFiles = fileListToImageFiles(event.dataTransfer.files);
+        if (imageFiles.length > 0) {
+          const insertPosition = view.posAtCoords({
+            left: event.clientX,
+            top: event.clientY,
+          })?.pos;
+
+          handleNewImageFiles(imageFiles, insertPosition);
+
+          // Return true to treat the event as handled. We call preventDefault
+          // ourselves for good measure.
+          event.preventDefault();
+          return true;
+        }
+
+        return false;
+      },
+      [handleNewImageFiles]
+    );
+
+  // Allow for pasting images
+  const handlePaste: NonNullable<EditorOptions["editorProps"]["handlePaste"]> =
+    useCallback(
+      (_view, event, _slice) => {
+        if (!event.clipboardData) {
+          return false;
+        }
+
+        const pastedImageFiles = fileListToImageFiles(
+          event.clipboardData.files
+        );
+        if (pastedImageFiles.length > 0) {
+          handleNewImageFiles(pastedImageFiles);
+        }
+
+        // We return false here to allow the standard paste-handler to run as
+        // well. This allows users to paste content containing both image files
+        // *and* text or html content.
+        return false;
+      },
+      [handleNewImageFiles]
+    );
 
   const [submittedContent, setSubmittedContent] = useState("");
 
@@ -46,6 +137,10 @@ export default function Editor() {
           extensions={extensions}
           content={exampleContent}
           editable={isEditable}
+          editorProps={{
+            handleDrop: handleDrop,
+            handlePaste: handlePaste,
+          }}
           renderControls={() => <EditorMenuControls />}
           RichTextFieldProps={{
             // The "outlined" variant is the default (shown here only as
