a08d0ad008a5b6a03c76e871e8976a21b6a4f077
Bryan Thomas <49354825+bryanjtc@users.noreply.github.com>
2023-12-30 12:53:51 -0500

    feat(icons): Add border style icons
    - Add BorderStyleDashed icon sourced from https://www.svgrepo.com/svg/361694/border-dashed
    - Add BorderStyleDotted icon sourced from https://www.svgrepo.com/svg/361691/border-dotted
    - Add BorderStyleSolid icon sourced from https://www.svgrepo.com/svg/361697/border-solid
    
    These icons are added to provide different border style options for UI elements.

--- src/icons/BorderStyleDashed.tsx
+++ src/icons/BorderStyleDashed.tsx
@@ -1,5 +1,6 @@
 import { createSvgIcon } from "@mui/material";
 
+// From: https://www.svgrepo.com/svg/361694/border-dashed
 const BorderStyleDashed = createSvgIcon(
   <path d="M6 13H1v-2h5zm6-2H7v2h5zm6 0h-5v2h5zm6 0h-5v2h5z" />,
   "BorderStyleDashed"
--- src/icons/BorderStyleSolid.tsx
+++ src/icons/BorderStyleSolid.tsx
@@ -1,5 +1,6 @@
 import { createSvgIcon } from "@mui/material";
 
+// From: https://www.svgrepo.com/svg/361697/border-solid
 const BorderStyleSolid = createSvgIcon(
   <path d="M23 13H1v-2h22z" />,
   "BorderStyleSolid"
