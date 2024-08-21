ddc0daadebb2bf0b84718149683f0fe087d55df7
Steven DeMartini <sjdemartini@users.noreply.github.com>
2024-08-20 17:06:44 -0700

    Handle lodash vs lodash-es for ESM

--- tsup.config.ts
+++ tsup.config.ts
@@ -45,5 +45,36 @@ export default defineConfig({
         }
       },
     },
+    {
+      // Use lodash with CJS and lodash-es with ESM. Unfortunately necessary per
+      // https://github.com/lodash/lodash/issues/5107, and this workaround is
+      // similarly done in several other projects (such as
+      // https://github.com/vueComponent/ant-design-vue/blob/2666cb79abf9825872e37b55d6d9d4c1cf30e62a/antd-tools/gulpfile.js#L136,
+      // https://github.com/casesandberg/react-color/blob/bc9a0e1dc5d11b06c511a8e02a95bd85c7129f4b/scripts/use-module-babelrc.js#L17-L23).
+      // Plugin adapted from the mui/icons-material plugin above, and
+      // https://github.com/refinedev/refine/blob/4c34444633a252fd830fc3ddbc5963e112269e2f/packages/shared/lodash-replace-plugin.ts.
+      name: "replaceLodashWithLodashEsForEsm",
+      setup: (build) => {
+        if (build.initialOptions.format === "esm") {
+          build.onEnd((args) => {
+            const lodashImportRegexp = /from\s*"lodash\/(\w*?)"/g;
+            const lodashEsmImport = 'from "lodash-es/$1.js"';
+
+            const jsOutputFiles =
+              args.outputFiles?.filter(
+                (el) => el.path.endsWith(".mjs") || el.path.endsWith(".js")
+              ) ?? [];
+
+            for (const jsOutputFile of jsOutputFiles) {
+              const str = new TextDecoder("utf-8").decode(
+                jsOutputFile.contents
+              );
+              const newStr = str.replace(lodashImportRegexp, lodashEsmImport);
+              jsOutputFile.contents = new TextEncoder().encode(newStr);
+            }
+          });
+        }
+      },
+    },
   ],
 });
