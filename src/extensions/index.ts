2441348f6814542aa31c6c276269ef7105b2ad7b
Steven DeMartini <sjdemartini@users.noreply.github.com>
2025-09-01 10:10:59 -0700

    Export utility classes for components
    Rather than having users hardcode "MuiTiptap-MenuButton-root", they can
    instead use `menuButtonClasses.root`, for instance. This is similar to
    MUI's pattern of exporting utility classes (e.g. `outlinedInputClasses`,
    `selectClasses`, etc.).

--- src/extensions/index.ts
+++ src/extensions/index.ts
@@ -8,9 +8,13 @@ export {
   scrollToCurrentHeadingAnchor,
   type HeadingWithAnchorOptions,
 } from "./HeadingWithAnchor";
+export { default as HeadingWithAnchorComponent } from "./HeadingWithAnchorComponent";
+export { headingWithAnchorComponentClasses } from "./HeadingWithAnchorComponent.classes";
 export {
   default as LinkBubbleMenuHandler,
   type LinkBubbleMenuHandlerStorage,
 } from "./LinkBubbleMenuHandler";
 export { default as ResizableImage } from "./ResizableImage";
+export { default as ResizableImageComponent } from "./ResizableImageComponent";
+export { resizableImageComponentClasses } from "./ResizableImageComponent.classes";
 export { default as TableImproved } from "./TableImproved";
--- src/index.ts
+++ src/index.ts
@@ -2,16 +2,21 @@ export {
   default as ControlledBubbleMenu,
   type ControlledBubbleMenuProps,
 } from "./ControlledBubbleMenu";
+export { controlledBubbleMenuClasses } from "./ControlledBubbleMenu.classes";
 export {
   default as LinkBubbleMenu,
   type LinkBubbleMenuProps,
 } from "./LinkBubbleMenu";
+export { linkBubbleMenuClasses } from "./LinkBubbleMenu/LinkBubbleMenu.classes";
 export { default as MenuBar, type MenuBarProps } from "./MenuBar";
+export { menuBarClasses } from "./MenuBar.classes";
 export { default as MenuDivider, type MenuDividerProps } from "./MenuDivider";
+export { menuDividerClasses } from "./MenuDivider.classes";
 export {
   default as RichTextContent,
   type RichTextContentProps,
 } from "./RichTextContent";
+export { richTextContentClasses } from "./RichTextContent.classes";
 export {
   default as RichTextEditor,
   type RichTextEditorProps,
@@ -25,6 +30,7 @@ export {
   default as RichTextField,
   type RichTextFieldProps,
 } from "./RichTextField";
+export { richTextFieldClasses } from "./RichTextField.classes";
 export {
   default as RichTextReadOnly,
   type RichTextReadOnlyProps,
@@ -33,6 +39,7 @@ export {
   default as TableBubbleMenu,
   type TableBubbleMenuProps,
 } from "./TableBubbleMenu";
+export { tableBubbleMenuClasses } from "./TableBubbleMenu.classes";
 export { RichTextEditorContext, useRichTextEditorContext } from "./context";
 export * from "./controls";
 export * from "./extensions";
