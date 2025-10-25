673c02a5c629ad94bba5785440f5696f993bafa5
Steven DeMartini <sjdemartini@users.noreply.github.com>
2025-10-25 15:12:16 -0700

    Configure type-import order instead of inferred
    https://github.com/simonhaenisch/prettier-plugin-organize-imports/pull/152

--- .prettierrc.cjs
+++ .prettierrc.cjs
@@ -1,4 +1,5 @@
 // https://github.com/simonhaenisch/prettier-plugin-organize-imports/issues/34#issuecomment-934676163
 module.exports = {
   plugins: [require.resolve("prettier-plugin-organize-imports")],
+  organizeImportsTypeOrder: "last",
 };
